270e479db37d0eac8f675061da05cb36f1c3075e
hfl15 <fanlinghuang@163.com>
2018-01-06 15:25:25 +0800

    update

---
+++ leetcode/src/344.cpp
@@ -0,0 +1,26 @@
+/*
+
+	[344. Reverse String - easy]
+
+	[description]
+	Write a function that takes a string as input and returns the string reversed.
+
+	[example]
+	Given s = "hello", return "olleh".	
+
+*/
+
+#inlcude <iostream>
+#inlcude <string>
+
+class Solution{
+public:
+	string reverseString(string s){
+
+	}
+}
+
+int main()
+{
+	return 0;
+}
